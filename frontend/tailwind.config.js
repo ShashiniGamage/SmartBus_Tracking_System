b14479a9a06c997a5c2e257ab3e9fe2d75571137
Shashini Gamage <ggshashini@gmail.com>
2026-06-25 00:58:36 +0530

    Update Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed: '#dc2626', // Attractive Red
+        darkRed: '#991b1b',
+        lightRed: '#fef2f2',
+        pureWhite: '#ffffff',
+        darkGray: '#1f2937',
+      }
+    },
   },
   plugins: [],
 }
