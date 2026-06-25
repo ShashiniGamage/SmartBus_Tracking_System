160b5e4cf9bdbbc5f4d2d49e2a70b66d3d2e4544
Shashini Gamage <ggshashini@gmail.com>
2026-06-26 00:36:30 +0530

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { register, login } = require('../controllers/authController');
 
-// Auth Controller eken functions deka import karaganima
-const { registerUser, loginUser } = require('../controllers/authController');
-
-//  (Register) - POST /api/auth/register
-router.post('/register', registerUser);
-
-//  (Login) - POST /api/auth/login
-router.post('/login', loginUser);
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
